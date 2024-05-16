7de6ac406b87e6bdbfefb00908a0438e84696bae
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-05-16 18:03:45 +1200

    DEAP

--- tricks/multiprocess_speedup.py
+++ tricks/multiprocess_speedup.py
@@ -1,12 +1,3 @@
-"""
-遗传编程在并行评估的过程中，存在一个瓶颈即快的个体需要等待慢的个体评估完成才能进入下一代，导致整体CPU利用率不高。
-
-实际上，在演化计算中，评估是可以异步执行的。遗传编程通常使用的编程范式是Generation-based，即每一代的个体需要等待所有个体评估完成才能进入下一代。
-
-但是，我们也可以使用Steady-state-based的编程范式，即每一个个体都是异步评估的，从而提高CPU利用率。
-
-这里，我将通过一个简单的例子，来展示出一下如何使用基于Python实现遗传编程的并行评估。
-"""
 import concurrent.futures
 import random
 import time
@@ -69,12 +60,6 @@ mstats.register("std", numpy.std)
 mstats.register("min", numpy.min)
 mstats.register("max", numpy.max)
 
-"""
-在本教程中，异步并行处理是通过ProcessPoolExecutor实现的。
-简单来说，原理是创建一个进程池，然后将评估任务提交给进程池，进程池会自动分配任务给空闲的进程。
-当任何一个进程完成评估任务时，我们可以获取其结果。如果新个体的适应度好于种群中的最差个体，我们可以将其加入种群，替换掉最差个体。
-当至少两个任务完成时，我们可以开始下一代的演化。实际上，开始下一代演化的条件是一个可以调节的参数，这里设置的越小，CPU利用率越高。
-"""
 def steady_state_gp(
     population,
     toolbox,
@@ -146,11 +131,6 @@ def steady_state_gp(
 
 
 if __name__ == "__main__":
-    """
-    最后，我们可以测试一下异步并行和传统的串行遗传编程算法的区别。
-    从下面的测试结果可以看出，异步并行的遗传编程算法的速度要快于传统的串行遗传编程算法。
-    异步并行的遗传编程算法只消耗了10秒，而传统的串行遗传编程算法消耗了16秒。
-    """
     start = time.time()
     population = toolbox.population(n=100)
     hof = tools.HallOfFame(1)
