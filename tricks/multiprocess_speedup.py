e30364df1f4df70a33cdf7646bb5e0e7262f090e
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-05-16 17:31:47 +1200

    DEAP

---
+++ tricks/multiprocess_speedup.py
@@ -0,0 +1,181 @@
+"""
+最近有小伙伴问到，遗传编程在并行评估的过程中，存在一个瓶颈即快的个体需要等待慢的个体评估完成才能进入下一代，导致整体CPU利用率不高。
+
+实际上，在演化计算中，评估是可以异步执行的。遗传编程通常使用的编程范式是Generation-based，即每一代的个体需要等待所有个体评估完成才能进入下一代。
+
+但是，我们也可以使用Steady-state-based的编程范式，即每一个个体都是异步评估的，从而提高CPU利用率。
+
+这里，我将通过一个简单的例子，来展示出一下如何使用基于Python实现遗传编程的并行评估。
+"""
+import concurrent.futures
+import random
+import time
+
+import numpy
+import numpy as np
+from deap import base, creator, gp
+from deap import tools
+from deap.algorithms import varAnd, eaSimple
+from deap.tools import selBest
+
+# 使用numpy创建一个数据集
+x = np.linspace(-10, 10, 100000)
+
+
+# 符号回归
+def evalSymbReg(ind):
+    func = toolbox.compile(ind)
+    # 评估生成的函数并计算MSE
+    mse = np.mean((func(x) - (x + 1) ** 2) ** 2)
+    return (mse,)
+
+
+# 创建个体和适应度函数
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+
+# 定义函数和终端变量
+pset = gp.PrimitiveSet("MAIN", arity=1)
+pset.addPrimitive(np.add, 2)
+pset.addPrimitive(np.subtract, 2)
+pset.addPrimitive(np.multiply, 2)
+pset.addPrimitive(np.negative, 1)
+
+
+def random_int():
+    return random.randint(-1, 1)
+
+
+pset.addEphemeralConstant("rand101", random_int)
+pset.renameArguments(ARG0="x")
+
+# 定义遗传编程操作
+toolbox = base.Toolbox()
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("compile", gp.compile, pset=pset)
+toolbox.register("evaluate", evalSymbReg)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr, pset=pset)
+
+# 定义统计指标
+stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+stats_size = tools.Statistics(len)
+mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+mstats.register("avg", numpy.mean)
+mstats.register("std", numpy.std)
+mstats.register("min", numpy.min)
+mstats.register("max", numpy.max)
+
+"""
+在本教程中，异步并行处理是通过ProcessPoolExecutor实现的。
+简单来说，原理是创建一个进程池，然后将评估任务提交给进程池，进程池会自动分配任务给空闲的进程。
+当任何一个进程完成评估任务时，我们可以获取其结果。如果新个体的适应度好于种群中的最差个体，我们可以将其加入种群，替换掉最差个体。
+当至少两个任务完成时，我们可以开始下一代的演化。实际上，开始下一代演化的条件是一个可以调节的参数，这里设置的越小，CPU利用率越高。
+"""
+def steady_state_gp(
+    population,
+    toolbox,
+    cxpb,
+    mutpb,
+    max_evaluations,
+    stats=None,
+    halloffame=None,
+    verbose=__debug__,
+):
+    logbook = tools.Logbook()
+    logbook.header = ["evals", "nevals"] + (stats.fields if stats else [])
+
+    executor = concurrent.futures.ProcessPoolExecutor()
+    futures = {}
+    evaluations = len(population)
+
+    # 评估初始种群
+    for i, ind in enumerate(population):
+        if not ind.fitness.valid:
+            ind.fitness.values = toolbox.evaluate(ind)
+    all_done = population
+
+    while evaluations < max_evaluations:
+        # 生成新个体
+        if evaluations + len(futures) <= max_evaluations and len(all_done) >= 2:
+            selected = toolbox.select(population, len(all_done))
+            offspring = varAnd(selected, toolbox, cxpb, mutpb)
+            all_done = []
+
+            # 提交评估任务
+            for child in offspring:
+                if evaluations + len(futures) <= max_evaluations:
+                    future = executor.submit(toolbox.evaluate, child)
+                    futures[future] = child
+                else:
+                    break
+
+        # 等待至少一个个体完成评估
+        current_done, _ = concurrent.futures.wait(
+            list(futures.keys()),
+            return_when=concurrent.futures.FIRST_COMPLETED,
+        )
+
+        # 处理评估完成的个体
+        done_inds = []
+        for future in current_done:
+            ind = futures.pop(future)
+            ind.fitness.values = future.result()
+            done_inds.append(ind)
+            all_done.append(ind)
+            evaluations += 1
+
+        if halloffame is not None:
+            halloffame.update(done_inds)
+
+        # 用高适应度个体替换低适应度个体
+        population = selBest(population + done_inds, len(population))
+
+        if verbose and evaluations % 100 == 0:
+            record = stats.compile(population) if stats else {}
+            logbook.record(evals=evaluations, **record)
+            print(logbook.stream)
+
+    executor.shutdown()
+    return population, logbook
+
+
+if __name__ == "__main__":
+    """
+    最后，我们可以测试一下异步并行和传统的串行遗传编程算法的区别。
+    从下面的测试结果可以看出，异步并行的遗传编程算法的速度要快于传统的串行遗传编程算法。
+    异步并行的遗传编程算法只消耗了2.1秒，而传统的串行遗传编程算法消耗了3.4秒。
+    """
+    start = time.time()
+    population = toolbox.population(n=100)
+    hof = tools.HallOfFame(1)
+    pop, log = steady_state_gp(
+        population=population,
+        toolbox=toolbox,
+        cxpb=0.9,
+        mutpb=0.1,
+        max_evaluations=10 * 100,
+        stats=mstats,
+        halloffame=hof,
+        verbose=True,
+    )
+    end = time.time()
+    print("time:", end - start)
+
+    population = toolbox.population(n=100)
+    hof = tools.HallOfFame(1)
+    pop, log = eaSimple(
+        population=population,
+        toolbox=toolbox,
+        cxpb=0.9,
+        mutpb=0.1,
+        ngen=10,
+        stats=mstats,
+        halloffame=hof,
+        verbose=True,
+    )
+    end = time.time()
+    print("time:", end - start)
